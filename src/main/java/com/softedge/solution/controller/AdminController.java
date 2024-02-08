4ccb5f851bbfaefc0a62515f7e433d7a5db38eed
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2024-02-08 14:05:16 +0300

    cross origin added

--- src/main/java/com/softedge/solution/controller/AdminController.java
+++ src/main/java/com/softedge/solution/controller/AdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = "/api/admin")
 public class AdminController extends BaseController {
 
